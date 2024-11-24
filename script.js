6a2cff24f56fa65c9111c1444944304e9030a08d
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2024-11-24 19:57:37 +0700

    change interface

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-let items = document.querySelectorAll('.slider .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-
-let active = 3;
-
-function loadShow() {
-    let stt = 0;
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1;
-    items[active].style.filter = 'none';
-    items[active].style.opacity = 1;
-    for (var i = active + 1; i < items.length; i++) {
-        stt++;
-        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-    stt = 0;
-    for (var i = active - 1; i >= 0; i--) {
-        stt++;
-        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-}
-loadShow();
-next.onclick = function() {
-    active = active + 1 < items.length ? active + 1 : active;
-    loadShow();
-}
-prev.onclick = function() {
-    active = active - 1 >= 0 ? active - 1 : active;
-    loadShow();
-}
+$('.element').each(function() {
+    $(this).mouseover(function() {
+        $(this).addClass('active');
+        $('.stage').children('.element').not('.active').addClass('inactive');
+    });
+    $(this).mouseleave(function() {
+        $(this).removeClass('active');
+        $('.stage').children('.element').not('.active').removeClass('inactive');
+    });
+});
