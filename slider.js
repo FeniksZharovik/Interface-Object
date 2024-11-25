6cdce5f29b11faaf06dda7ae5540beac2fe4ff5b
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2024-11-25 07:21:07 +0700

    D assets, M slider & style

--- slider.js
+++ slider.js
@@ -13,4 +13,25 @@ $(document).ready(function() {
     });
 
     // -------
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const images = document.querySelectorAll('.home-image, .wellcome-image');
+
+        function checkVisibility() {
+            images.forEach(image => {
+                const rect = image.getBoundingClientRect();
+                if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                    image.style.opacity = 1;
+                    image.style.animationPlayState = 'running';
+                } else {
+                    image.style.opacity = 0;
+                    image.style.animationPlayState = 'paused';
+                }
+            });
+        }
+
+        window.addEventListener('scroll', checkVisibility);
+        window.addEventListener('resize', checkVisibility);
+        checkVisibility(); // Initial check
+    });
 });
